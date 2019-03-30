fa134cf140275c665d982fc0bca1f660a0872dde
tblais1224 <thomasblais.student@careerdevs.com>
2019-03-30 12:20:17 -0400

    version5

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-//going to put everything from requirments into objects
+//going to put everything from requirements into objects
 
 var todoList = {
     todos: [],
   
     displayTodos: function() {
       //dont need to name function in method because it has key value name
-      console.log("My todos:", this.todos);
+      if (this.todos.length === 0) {
+        console.log("Your todo list is empty!");
+      } else {
+        console.log("My todos:");
+        for (let i = 0; i < this.todos.length; i++) {
+          if (this.todos[i].completed === true) {
+            console.log("(x)", this.todos[i].todoText);
+          } else {
+            console.log("()", this.todos[i].todoText);
+          }
+        }
+      }
     },
   
     addTodo: function(todoText) {
