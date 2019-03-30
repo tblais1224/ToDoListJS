ca5724072ffdaf44a7fea91217401801ef4908bd
tblais1224 <thomasblais.student@careerdevs.com>
2019-03-30 12:17:20 -0400

    fourth commit

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
 //going to put everything from requirments into objects
 
 var todoList = {
-    todos: ["item 1", "item 2", "item 3", "item 4"],
-
-    displayTodos: function() { //dont need to name function in method because it has key value name
-        console.log('My todos:', this.todos);
+    todos: [],
+  
+    displayTodos: function() {
+      //dont need to name function in method because it has key value name
+      console.log("My todos:", this.todos);
     },
-
-    addTodo: function(todo) {
-        this.todos.push(todo);
-        this.displayTodos();
+  
+    addTodo: function(todoText) {
+      this.todos.push({
+        todoText: todoText,
+        completed: false
+      });
+      this.displayTodos();
     },
-
-    changeTodo: function(position, newValue) {
-        this.todos[position] = newValue;
-        this.displayTodos();
+  
+    changeTodo: function(position, todoText) {
+      this.todos[position].todoText = todoText;
+      this.displayTodos();
     },
-
+  
     deleteTodos: function(position) {
-        this.todos.splice(position, 1);
-        this.displayTodos();
+      this.todos.splice(position, 1);
+      this.displayTodos();
+    },
+  
+    toggleCompleted: function(position) {
+      var todo = this.todos[position];
+      todo.completed = !todo.completed;
+      this.displayTodos();
     }
-};
+  };
+  
